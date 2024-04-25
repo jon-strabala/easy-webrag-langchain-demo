4b959a4b51547e6eb4c3e72bc57e799cad130866
Jon Strabala <54073900+jon-strabala@users.noreply.github.com>
2024-04-25 13:02:43 -0700

    Check CB version

--- check_couchbase.sh
+++ check_couchbase.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-PATH=${CB_HOME}:$PATH
-
 # Check if all required environment variables are set
 required_vars=( "CB_HOSTNAME" "CB_USERNAME" "CB_PASSWORD" )
 
+echo ""
 echo "Verifying database conectivity via evars ${required_vars[@]}"
 
 doexit=0
@@ -18,7 +17,6 @@ if [ "$doexit" = "1" ]; then
     exit 1
 fi
 
-
 curl -s -k -u ${CB_USERNAME}:${CB_PASSWORD} https://${CB_HOSTNAME}:18093/admin/ping | grep '"OK"'
 exit_code=$?
 if [ $exit_code = "0" ] ; then
@@ -27,3 +25,18 @@ else
     echo "ERROR: Unable to access Couchbase Server, check your environamt variables and your server instance"
 fi
 
+echo ""
+versionstr=`curl -s -k -u ${CB_USERNAME}:${CB_PASSWORD} https://${CB_HOSTNAME}:18091/pools/default | grep -o '"version":"[^"]*' | head -1`
+
+# Extract version string from JSON
+version=$(echo "$versionstr" | grep -oP '(?<="version":")[^"]+')
+
+# Check if version is greater than or equal to 7.6
+if [[ "$version" == "7.6" || "$version" > "7.6" ]]; then
+    echo "Your Couchbase Version <$version> is 7.6 or later, GOOD you can run vector search appications"
+else
+    echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+    echo "Your Couchbase Version <$version> is earlier than 7.6, ERROR sorry you can't run vector search appications"
+    echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+fi
+echo ""
