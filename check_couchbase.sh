96982133562d963f363b1a1e915df012ae74f176
jon-strabala <jon.strabala@couchbase.com>
2024-06-11 13:02:39 -0700

    improve connectivy checking

--- check_couchbase.sh
+++ check_couchbase.sh
@@ -2,12 +2,19 @@
 
 # Check if all required environment variables are set
 required_vars=( "CB_HOSTNAME" "CB_USERNAME" "CB_PASSWORD" )
+required_ftsvars=( "CB_FTSHOSTNAME" "CB_USERNAME" "CB_PASSWORD" )
 
 echo ""
-echo "1) Verifying database connectivity via evars ${required_vars[@]}:"
+echo "1) Verifying data service connectivity via evars ${required_vars[@]}:"
 
 doexit=0
 for var in "${required_vars[@]}"; do
+    if [[ -z "${!var}" ]]; then
+        echo "Error: $var is not set."
+        doexit=1
+    fi
+done
+for var in "${required_ftsvars[@]}"; do
     if [[ -z "${!var}" ]]; then
         echo "Error: $var is not set."
         doexit=1
@@ -17,12 +24,27 @@ if [ "$doexit" = "1" ]; then
     exit 1
 fi
 
-curl -s -k -u ${CB_USERNAME}:${CB_PASSWORD} https://${CB_HOSTNAME}:18093/admin/ping | grep '"OK"'
+# QUERY PING (not needed)
+# curl -s -k -u ${CB_USERNAME}:${CB_PASSWORD} https://${CB_HOSTNAME}:18093/admin/ping | grep '"OK"'
+
+# MANAGEMENT PING
+curl -s -k -u ${CB_USERNAME}:${CB_PASSWORD} https://${CB_HOSTNAME}:18091/pools | grep "implementationVersion" > /dev/null
 exit_code=$?
 if [ $exit_code = "0" ] ; then
-    echo "Access succeeded, the Couchbase Server is up and running"
+    echo "Access succeeded (port 18091), the Couchbase Server (data service) is up and running"
 else
-    echo "ERROR: Unable to access Couchbase Server, check your environamt variables and your server instance"
+    echo "ERROR: Unable to access Couchbase Server (port 18091), check your environamt variables and your server instance"
+fi
+
+echo ""
+echo "2) Verifying search service connectivity via evars ${required_ftsvars[@]}:"
+# SEARCH PING
+curl -s -k -u ${CB_USERNAME}:${CB_PASSWORD} https://${CB_FTSHOSTNAME}:18094/api/cfg | grep "status" | grep "ok" > /dev/null
+exit_code=$?
+if [ $exit_code = "0" ] ; then
+    echo "Access succeeded (port 18094), the Couchbase Server (search service) is up and running"
+else
+    echo "ERROR: Unable to access Couchbase Server (port 18094), check your environamt variables and your server instance"
 fi
 
 echo ""
@@ -32,7 +54,7 @@ versionstr=`curl -s -k -u ${CB_USERNAME}:${CB_PASSWORD} https://${CB_HOSTNAME}:1
 version=$(echo "$versionstr" | grep -oP '(?<="version":")[^"]+')
 
 # Check if version is greater than or equal to 7.6
-echo "2) Checking your cluster version:"
+echo "3) Checking your cluster version:"
 if [[ "$version" == "7.6" || "$version" > "7.6" ]]; then
     echo "Your Couchbase Version <$version> is 7.6 or later, GOOD you can run vector search applications"
 else
@@ -41,3 +63,4 @@ else
     echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 fi
 echo ""
+
